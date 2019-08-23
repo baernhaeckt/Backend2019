4f0a94fc08c6169598408b1b1e3e9444a664b9f5
Oliver Hofmann <oliver.hofmann@garaio.com>
2019-08-23 23:36:52 +0200

    Add Security Classes.

---
+++ Core/Security/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Security
+{
+    public class User
+    {
+        public string Email { get; set; }
+    }
+}
