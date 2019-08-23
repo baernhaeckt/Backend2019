ef349747f10a90a74604da21aacd598ced6c55ef
Christoph Seiler <christoph.seiler@gmail.com>
2019-08-23 22:52:48 +0200

    feat: add first models and token controller and a test project, because every project needs a test project.

---
+++ Backend/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Backend.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public TokenResponse Get(Guid tokenGuid)
+        {
+            return new TokenResponse()
+            {
+                Id = Guid.NewGuid(),
+                Text = "default text",
+                Points = 100,
+                Valid = true,
+                User = new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "test@mail.com"
+                }
+            };
+        }
+    }
+}
---
+++ Backend/Models/TokenResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Backend.Controllers
+{
+    public class TokenResponse
+    {
+        public User User { get; set; }
+
+        public Guid Id { get; set; }
+
+        public String Text { get; set; }
+
+        public int Points { get; set; }
+
+        public bool Valid { get; set; }
+    }
+}
