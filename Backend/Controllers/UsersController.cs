2c049153199cd197b51e39b074296b9846d54ab1
Christoph Seiler <christoph.seiler@gmail.com>
2019-08-23 23:04:55 +0200

    Add UsersController.

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Backend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("current")]
+        public UserResponse Current()
+        {
+            return new UserResponse()
+            {
+                Id = Guid.NewGuid(),
+                Email = "current@test.ch"
+            };
+        }
+
+        [HttpGet]
+        public UserResponse Get(Guid guid)
+        {
+            return new UserResponse()
+            {
+                Id = guid,
+                Email = "user@test.ch"
+            };
+        }
+    }
+}
