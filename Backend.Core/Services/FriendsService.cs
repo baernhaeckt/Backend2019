01506a1d0b758fd31db352d07c6d51880e600247
Christoph Seiler <christoph.seiler@gmail.com>
2019-08-24 19:08:18 +0200

    make FriendService symetric.

--- Backend.Core/Services/FriendsService.cs
+++ Backend.Core/Services/FriendsService.cs
@@ -22,12 +22,17 @@ namespace Backend.Core.Services
                 throw new WebException("email must not be empty", System.Net.HttpStatusCode.BadRequest);
             }
 
-            User friendUser = UserRepository.GetQuerableAsync().SingleOrDefault(u => u.Email == Principal.Email());
+            User friendUser = UserRepository.GetQuerableAsync().SingleOrDefault(u => u.Email == friendEmail);
 
             if (friendUser == null)
             {
                 // TODO: Invite to Platform
                 throw new WebException($"no user with email: {friendEmail} found.", System.Net.HttpStatusCode.BadRequest);
+            }
+
+            if (CurrentUser.Id == friendUser.Id)
+            {
+                throw new WebException($"can't be your own friend.", System.Net.HttpStatusCode.BadRequest);
             }
 
             var friends = (CurrentUser.Friends ?? Enumerable.Empty<string>()).ToList();
@@ -39,14 +44,20 @@ namespace Backend.Core.Services
             friends.Add(friendUser.Id);
             var user = CurrentUser;
             user.Friends = friends;
-
             await UserRepository.UpdateAsync(user.Id, user);
+
+            var friendsFriend = (friendUser.Friends ?? Enumerable.Empty<string>()).ToList();
+            if (!friendsFriend.Contains(CurrentUser.Id))
+            {
+                friendsFriend.Add(CurrentUser.Id);
+                friendUser.Friends = friendsFriend;
+                await UserRepository.UpdateAsync(friendUser.Id, friendUser);
+            }
         }
 
         public async Task RemoveFriend(string friendUserId)
         {
-            User user = UserRepository.GetQuerableAsync()
-                .Single(u => u.Email.Equals(Principal.Email(), StringComparison.InvariantCultureIgnoreCase));
+            User user = CurrentUser;
 
             var friends = user.Friends.ToList();
             if (!friends.Contains(friendUserId))
@@ -56,8 +67,17 @@ namespace Backend.Core.Services
 
             friends.Remove(friendUserId);
             user.Friends = friends;
-
             await UserRepository.UpdateAsync(user.Id, user);
+
+            User exFriend = await UserRepository.GetByIdAsync(friendUserId);
+
+            var exFriendFriends = exFriend.Friends.ToList();
+            if (exFriendFriends.Contains(CurrentUser.Id))
+            {
+                exFriendFriends.Remove(CurrentUser.Id);
+                exFriend.Friends = exFriendFriends;
+                await UserRepository.UpdateAsync(exFriend.Id, exFriend);
+            }
         }
 
         public IEnumerable<User> Friends
