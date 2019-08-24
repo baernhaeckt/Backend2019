2f92367b0a00b60aaec766254d35707f638de493
Christoph Seiler <christoph.seiler@gmail.com>
2019-08-24 14:00:44 +0200

    feat: add mongodb entities.

---
+++ Backend.Database/Widgets/Quiz/UserQuizAnswer.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Database.Widgets.Quiz
+{
+    public class UserQuizAnswer
+    {
+        public string QuizQuestionId { get; set; }
+
+        public string SelectedAnswer { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+        public int Points { get; set; }
+    }
+}
