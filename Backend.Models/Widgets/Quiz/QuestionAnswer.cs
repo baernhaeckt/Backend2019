7d102fce295b049dfb7c7dd703f7df9151e2fa0b
Christoph Seiler <christoph.seiler@gmail.com>
2019-08-24 14:02:52 +0200

    feat: add quiz models.

---
+++ Backend.Models/Widgets/Quiz/QuestionAnswer.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models.Widgets.Quiz
+{
+    public class QuestionAnswer
+    {
+        public string QuestionId { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
---
+++ Backend.Models/Widgets/Quiz/QuestionResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Backend.Models.Widgets.Quiz
+{
+    public class QuestionResponse
+    {
+        public string Id { get; set; }
+
+        public string Question { get; set; }
+
+        public IEnumerable<string> Answers { get; set; }
+    }
+}
