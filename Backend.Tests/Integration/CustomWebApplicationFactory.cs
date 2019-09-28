7482ba85ee94883cda98ec96b0e05336db5bbc65
Marc Sallin <marc.sallin@outlook.com>
2019-09-28 18:22:42 +0200

    Setup for in-memory integration tests

---
+++ Backend.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace Backend.Tests.Integration
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment(Environments.Development);
+            return base.CreateHost(builder);
+        }
+    }
+}
