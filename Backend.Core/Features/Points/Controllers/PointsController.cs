2a58c7495b8bd458dd96932a686d278a7fdba5b3
Marc Sallin <marc.sallin@outlook.com>
2019-10-02 14:04:13 +0200

    Use current user to get the points

--- Backend.Core/Features/Points/Controllers/PointsController.cs
+++ Backend.Core/Features/Points/Controllers/PointsController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.Core.Entities;
+using Backend.Core.Extensions;
 using Backend.Core.Features.Points.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Core.Features.Points.Controllers
 {
-    [Route("api/users/{userId}/points")] // TODO: Remove this route
     [Route("api/points")]
     [ApiController]
     public class PointsController : ControllerBase
@@ -18,9 +18,9 @@ namespace Backend.Core.Features.Points.Controllers
         public PointsController(PointService pointService) => _pointService = pointService;
 
         [HttpGet]
-        public async Task<IEnumerable<PointResponse>> Get(Guid userId)
+        public async Task<IEnumerable<PointResponse>> Get()
         {
-            IEnumerable<PointAction> pointList = await _pointService.GetPointHistory(userId);
+            IEnumerable<PointAction> pointList = await _pointService.GetPointHistory(HttpContext.User.Id());
             return pointList.OrderByDescending(p => p.Date).Take(25).Select(p => new PointResponse
             {
                 Date = p.Date,
