0356caff1fcf3d204af354d504f10f645cb1ca17
Marc Sallin <marc.sallin@outlook.com>
2019-10-20 11:59:05 +0200

    Add logging for SignInQueryHandlers

--- Backend.Core/Features/Partner/Queries/SignInQueryHandler.cs
+++ Backend.Core/Features/Partner/Queries/SignInQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Backend.Core.Entities;
 using Backend.Infrastructure.Persistence.Abstraction;
 using Backend.Infrastructure.Security.Abstraction;
+using Microsoft.Extensions.Logging;
 using Silverback.Messaging.Subscribers;
 
 namespace Backend.Core.Features.Partner.Queries
@@ -15,30 +16,38 @@ namespace Backend.Core.Features.Partner.Queries
 
         private readonly ISecurityTokenFactory _securityTokenFactory;
 
-        public SignInQueryHandler(IReader reader, IPasswordStorage passwordStorage, ISecurityTokenFactory securityTokenFactory)
+        private readonly ILogger<SignInQueryHandler> _logger;
+
+        public SignInQueryHandler(IReader reader, IPasswordStorage passwordStorage, ISecurityTokenFactory securityTokenFactory, ILogger<SignInQueryHandler> logger)
         {
             _reader = reader;
             _passwordStorage = passwordStorage;
             _securityTokenFactory = securityTokenFactory;
+            _logger = logger;
         }
 
         public async Task<SignInQueryResult> ExecuteAsync(SignInQuery query)
         {
-            Tuple<Guid, string, string> result = await _reader.GetByIdOrThrowAsync<TokenIssuer, Tuple<Guid, string, string>>(
+            _logger.PartnerSignInInitiated(query.Id);
+
+            (Guid id, string? name, string? hash) = await _reader.GetByIdOrThrowAsync<TokenIssuer, Tuple<Guid, string, string>>(
                 query.Id, u => new Tuple<Guid, string, string>(u.Id, u.Name, u.SecretHash));
 
-            if (result == null)
+            if (id == Guid.Empty)
             {
+                _logger.PartnerSignInPartnerNotFound(query.Id);
                 return new SignInQueryResult(true, false, string.Empty);
             }
 
-            bool passwordOk = _passwordStorage.Match(query.Secret, result.Item3);
+            bool passwordOk = _passwordStorage.Match(query.Secret, hash);
             if (!passwordOk)
             {
+                _logger.PartnerSignInPasswordMismatch(query.Id);
                 return new SignInQueryResult(false, true, string.Empty);
             }
 
-            string token = _securityTokenFactory.Create(result.Item1, result.Item2, new[] { Roles.Partner });
+            _logger.PartnerSignInSuccessful(query.Id, name);
+            string token = _securityTokenFactory.Create(id, name, new[] { Roles.Partner });
             return new SignInQueryResult(false, false, token);
         }
     }
--- Backend.Core/Features/UserManagement/Queries/SignInQueryHandler.cs
+++ Backend.Core/Features/UserManagement/Queries/SignInQueryHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Backend.Core.Entities;
 using Backend.Infrastructure.Persistence.Abstraction;
 using Backend.Infrastructure.Security.Abstraction;
+using Microsoft.Extensions.Logging;
 using Silverback.Messaging.Subscribers;
 
 namespace Backend.Core.Features.UserManagement.Queries
@@ -16,30 +17,38 @@ namespace Backend.Core.Features.UserManagement.Queries
 
         private readonly ISecurityTokenFactory _securityTokenFactory;
 
-        public SignInQueryHandler(IReader reader, IPasswordStorage passwordStorage, ISecurityTokenFactory securityTokenFactory)
+        private readonly ILogger<SignInQueryHandler> _logger;
+
+        public SignInQueryHandler(IReader reader, IPasswordStorage passwordStorage, ISecurityTokenFactory securityTokenFactory, ILogger<SignInQueryHandler> logger)
         {
             _reader = reader;
             _passwordStorage = passwordStorage;
             _securityTokenFactory = securityTokenFactory;
+            _logger = logger;
         }
 
         public async Task<SignInQueryResult> ExecuteAsync(SignInQuery query)
         {
+            _logger.UserSignInInitiated(query.Email);
+
             Tuple<Guid, string, string, IEnumerable<string>>? userByEmail = await _reader.SingleOrDefaultAsync<User, Tuple<Guid, string, string, IEnumerable<string>>>(
                 u => u.Email == query.Email.ToLowerInvariant(),
                 u => new Tuple<Guid, string, string, IEnumerable<string>>(u.Id, u.PasswordHash, u.Email, u.Roles));
 
             if (userByEmail == null)
             {
+                _logger.UserSignInUserNotFound(query.Email);
                 return new SignInQueryResult(true, false, string.Empty);
             }
 
             bool passwordOk = _passwordStorage.Match(query.Password, userByEmail.Item2);
             if (!passwordOk)
             {
+                _logger.UserSignInPasswordMismatch(query.Email);
                 return new SignInQueryResult(false, true, string.Empty);
             }
 
+            _logger.UserSignInSuccessful(query.Email);
             string token = _securityTokenFactory.Create(userByEmail.Item1, userByEmail.Item3, userByEmail.Item4);
             return new SignInQueryResult(false, false, token);
         }
